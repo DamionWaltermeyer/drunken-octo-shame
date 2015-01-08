a20b1b9ec37a12c34b33b6a50e3e9b850bef5bd3
DamionWaltermeyer <Damionbulk@yahoo.com>
2015-01-08 11:16:36 -0500

    Create restart-on-error.sh
    Bash script/service to attempt to restart docker containers that have exited because they felt like it.

---
+++ restart-on-error.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Every 120 seconds this script attempts to restart any docker containers that 
+#have failed with a -1 exit code in a time frame greater than one but less than 60 minutes.
+#Pair this up with a configuration manager to put on docker hosts and run at boot. 
+#If you find you have another frequently occuring exit code, just change this and have it run twice.
+#log size estimated to be ~10M per month , change sleep to effect frequency & log size.
+#manual usage:   restart-on-error.sh &>> DockerRestart.log &
+while true; do docker start $(docker ps -a |grep '(-1)'| grep minutes|cut -d" " -f 1); sleep 120; done
